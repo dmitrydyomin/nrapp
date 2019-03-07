18069b7035e924f20dd58c128803810d0d7b71aa
Dmitry Dyomin <dma01x@gmail.com>
2019-03-07 11:53:57 +0300

    Reverting eslint install command

--- index.sh
+++ index.sh
@@ -15,8 +15,7 @@ rm -rf src public README.md .git
 ESLINT_VERSION=$(grep '"eslint"' node_modules/react-scripts/package.json | sed 's/[a-z": ,]//g')
 
 npm i axios body-parser bookshelf bootstrap classnames express express-async-handler final-form knex lodash mysql2 prop-types qs ramda react-final-form react-icons react-router react-router-dom react-select react-toastify reactstrap ws
-npm i -D concurrently eslint eslint-config-airbnb eslint-plugin-import eslint-plugin-jsx-a11y eslint-plugin-react http-proxy-middleware nodemon
-npm r eslint && npm i -D eslint@$ESLINT_VERSION
+npm i -D concurrently eslint@$ESLINT_VERSION eslint-config-airbnb eslint-plugin-import eslint-plugin-jsx-a11y eslint-plugin-react http-proxy-middleware nodemon
 
 sed -i '' 's/"start": "react-scripts start",/"backend": "nodemon --watch src\/backend src\/backend",\
     "frontend": "react-scripts start",\
